fef8df090859ca2d7b3e5751378860cc4ad8cfa0
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2021-10-17 11:33:02 -0500

    commit to heroku

--- knexfile.js
+++ knexfile.js
@@ -45,30 +45,3 @@ module.exports = {
     },
   },
 };
-
-const pgPool = new Pool(pgConfig);
-const pgPoolWrapper = {
-  async connect() {
-    for (let nRetry = 1; ; nRetry++) {
-      try {
-        const client = await pgPool.connect();
-        if (nRetry > 1) {
-          console.info("Now successfully connected to Postgres");
-        }
-        return client;
-      } catch (e) {
-        if (e.toString().includes("ECONNREFUSED") && nRetry < 5) {
-          console.info(
-            "ECONNREFUSED connecting to Postgres, " +
-              "maybe container is not ready yet, will retry " +
-              nRetry
-          );
-          // Wait 1 second
-          await new Promise((resolve) => setTimeout(resolve, 1000));
-        } else {
-          throw e;
-        }
-      }
-    }
-  },
-};
