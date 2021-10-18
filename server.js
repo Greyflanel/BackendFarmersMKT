e20e822ec3f68d19a7f20b5a74add4604a798b0a
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2021-10-18 12:48:39 -0500

    knexfile

--- server.js
+++ server.js
@@ -28,27 +28,6 @@ require("dotenv").config();
 
 const PORT = process.env.PORT;
 
-const { Client } = require("pg");
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-client.connect();
-
-client.query(
-  "SELECT table_schema,table_name FROM information_schema.tables;",
-  (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  }
-);
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
