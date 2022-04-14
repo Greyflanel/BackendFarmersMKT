821a1e36961af158b00fdf18115423f8061d78ca
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2022-04-14 10:56:15 -0500

    working on stripe functionality

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const { join } = require("path");
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+const uuid = require("uuid/v4");
 const authConfig = require("./auth_config.json");
 require("dotenv").config();
 
@@ -36,14 +37,12 @@ server.use(logger('short'));
 server.use(express.static(join(__dirname, "build")));
 server.use(cors({ origin: '*' }));
 
-
-
 const productRouter = require("./products/products-router");
 
 server.use("/", productRouter);
 
 const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
+    secret: jwksRsa.expressJwtSecret({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
@@ -55,22 +54,34 @@ const checkJwt = jwt({
   algorithms: ["RS256"],
 });
 
-server.get("/msg", checkJwt, (req, res) => {
+server.get("/external-api", checkJwt, (req, res) => {
   res.send({
     msg: "Your access token was successfully validated!",
   });
 });
 
-server.post("/payment", cors(), async (req, res) => {
-  let { amount, id } = req.body
+server.post("/payment", checkJwt, cors(), async (req, res) => {
+  let { product, amount, id, token } = req.body;
+  let idempontencyKey = uuid();
+  console.log("PRODUCT", product)
+  console.log("PRICE", product.price)
   try {
     const payment = await stripe.paymentIntents.create({
-      amount,
+      amount: product.price * 100,
       currency: "USD",
       description: "Computer Parts and Electronics",
       payment_method: id,
-      confirm: true
-    })
+      confirm: true,
+      email: token.email,
+      receipt_email: token.email,
+      description: product.product_details,
+      shipping: {
+        name: token.card.name,
+        address: {
+          country: token.card.address_country
+        }
+      }
+    }, {idempontencyKey})
     console.log("Payment", payment)
     res.json({ 
       message: "Payment Successful!"
@@ -79,10 +90,11 @@ server.post("/payment", cors(), async (req, res) => {
     console.log("Error", error)
     res.json({
       message: "Payment Failed",
-      succes: false
+      success: false
     })
 
   }
+  
 })
 
 server.listen(PORT, () => {
